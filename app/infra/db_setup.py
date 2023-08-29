05a75fd634f428b9a3040de1ec11aba5f0e45fc9
Levan Samadashvili <lsama19@freeuni.edu.ge>
2023-08-29 14:08:45 +0400

    update db_setup for new table and repositories

--- app/infra/db_setup.py
+++ app/infra/db_setup.py
@@ -9,6 +9,7 @@ from sqlite3 import Connection, Cursor
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = Path(BASE_DIR).joinpath("..").joinpath("app.db")
 
+
 @dataclass
 class ConnectionProvider:
     _connection: Connection | None = field(default=None, init=False)
@@ -22,20 +23,26 @@ class ConnectionProvider:
 
 
 def create_tables(cursor: Cursor, connection: Connection) -> None:
-    # cursor.execute("DROP TABLE IF EXISTS accounts;")
-    #
-    # cursor.execute(
-    #     "CREATE TABLE IF NOT EXISTS accounts (id INT, username TEXT, "
-    #     "password TEXT, token TEXT, token_is_valid INT);"
-    # )
-
+    cursor.execute("DROP TABLE IF EXISTS account;")
     cursor.execute("DROP TABLE IF EXISTS company;")
 
     cursor.execute(
+        "CREATE TABLE IF NOT EXISTS account "
+        "(username TEXT PRIMARY KEY,"
+        " password TEXT);"
+    )
+
+    cursor.execute(
         "CREATE TABLE IF NOT EXISTS company "
-        "(id INTEGER PRIMARY KEY, company_name TEXT, "
-        "website TEXT, industry TEXT, organization_size INT, "
-        "image_uri TEXT, cover_image_uri TEXT);"
+        "(id INTEGER PRIMARY KEY,"
+        " company_name TEXT, "
+        " website TEXT, "
+        " industry TEXT,"
+        " organization_size INTEGER, "
+        " image_uri TEXT,"
+        " cover_image_uri TEXT, "
+        " owner_username TEXT, "
+        " FOREIGN KEY(owner_username) REFERENCES account(username));"
     )
 
     connection.commit()
